e0924df9d40f777edb2518f43db9a6321b4c97d9
pengwan <pwan@aerohive.com>
2015-10-04 14:16:00 +0800

    modify

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,29 @@
 var React = require('react');
 
+var Parse = require('parse');
+var ParseReact = require('parse-react');
+
 var Comment = React.createClass({
+
+    mixins : [ParseReact.Mixin],
+
+    observe : function(){
+        comments : (new Parse.Query('users'))
+    },
+
     render : function(){
         return (
             <h1>Parse Demo</h1>       
+            <ul>
+                {
+                    this.data.comments.map(function(item){
+                        return <li>{item.objectId}</li>;
+                    })
+                }
+            </ul>
         );
     } 
+
 });
 
 React.render(<Comment />, document.body);
